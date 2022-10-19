c4494d8422747a2680d672a380deddc374353643
stevebottos <bottos.steve@gmail.com>
2022-10-19 19:30:28 +0000

    trying out a new architecture to hopefully remove blotchiness

--- models/dcgan_upsample.py
+++ models/dcgan_upsample.py
@@ -33,11 +33,10 @@ class Generator(nn.Module):
         self.conv5 = UpBlock(conv_dim * 5, conv_dim * 4)
         self.conv6 = UpBlock(conv_dim * 4, conv_dim * 3)
         self.final = nn.Sequential(
-            nn.Upsample(68, mode="bilinear", align_corners=True),
-            nn.Conv2d(conv_dim * 3, conv_dim * 3, kernel_size=3, padding=0, bias=False),
+            nn.Conv2d(conv_dim * 3, conv_dim * 3, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(conv_dim * 3),
             nn.ReLU(inplace=True),
-            nn.Conv2d(conv_dim * 3, channels, kernel_size=3, padding=0, bias=False),
+            nn.Conv2d(conv_dim * 3, channels, kernel_size=3, padding=1, bias=False),
             nn.Tanh(),
         )
 
